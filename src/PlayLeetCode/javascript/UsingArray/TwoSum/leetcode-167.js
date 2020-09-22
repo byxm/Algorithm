a8787a060468ac5835ec43e862903eb0324378d7
陈攀宇 <chenpy@knownsec.com>
2020-09-22 23:28:50 +0800

    feat: leetcode 167题,暴力解法和二分查找解法

---
+++ src/PlayLeetCode/javascript/UsingArray/TwoSum/leetcode-167.js
@@ -0,0 +1,52 @@
+"use strict";
+/**
+ * @description leetcode第167题
+ */
+exports.__esModule = true;
+// 暴力解法，时间复杂度O(n*2)
+// function twoSum(numbers: number[], target: number): number[] {
+//     for(let i = 0; i < numbers.length; i++) {
+//         const firstEle = numbers[i]
+//         for(let j = 1; j < numbers.length; j++) {
+//             const secondEle = numbers[j]
+//             if(firstEle + secondEle === target) {
+//                 i++
+//                 j++
+//                 return [i,j]
+//             }
+//         }
+//     }
+// };
+// 结合二分查找时间复杂度O(nlogn)
+// 先找到第一个位置的元素，然后对剩余有序数组进行二分查找匹配
+function twoSum(numbers, target) {
+    for (var i = 0; i < numbers.length; i++) {
+        var addFirst = numbers[i];
+        var addSecond = target - addFirst;
+        var res = BinarySearch(numbers, i + 1, numbers.length, addSecond);
+        console.log('res', res);
+        if (res > -1) {
+            i += 1;
+            return [i, res];
+        }
+    }
+}
+function BinarySearch(arr, startIndex, endIndex, target) {
+    //   console.log('searchArr', startIndex, endIndex, target);
+    var mid = Math.floor((startIndex + endIndex) / 2);
+    if (arr[mid] === target) {
+        mid += 1;
+        return mid;
+    }
+    if (startIndex === endIndex) {
+        return -1;
+    }
+    if (arr[mid] > target) {
+        return BinarySearch(arr, startIndex, mid - 1, target);
+    }
+    if (arr[mid] < target) {
+        return BinarySearch(arr, mid + 1, endIndex, target);
+    }
+}
+var result = twoSum([3, 24, 50, 79, 88, 150, 345], 200);
+console.log("result", result);
