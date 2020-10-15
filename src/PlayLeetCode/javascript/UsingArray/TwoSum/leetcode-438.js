c3fdde227f496ab79ad88d6d95b98e63a49579bb
陈攀宇 <chenpy@knownsec.com>
2020-10-15 09:37:47 +0800

    feat: leetcode-438问题优化解法

--- src/PlayLeetCode/javascript/UsingArray/TwoSum/leetcode-438.js
+++ src/PlayLeetCode/javascript/UsingArray/TwoSum/leetcode-438.js
@@ -4,7 +4,8 @@
  * @return {number[]}
  */
 
- // 这个答案思路正确，但是写的相当繁琐。原因在于计算子串元素出现的次数
+
+// 这个答案思路正确，但是写的相当繁琐。原因在于计算子串元素出现的次数
 var findAnagrams = function (s, p) {
   let l = 0;
   let r = 0;
@@ -68,4 +69,45 @@ var findAnagrams = function (s, p) {
   return res;
 };
 
-console.log(findAnagrams("abab", "ab"));
+// console.log(findAnagrams("Abab", "Aba"));
+
+/**
+ * 上述解法逻辑写的复杂的原因在于统计遍历的子串中各个字母相应的出现顺序
+ * 因为题目中说明了只会出现小写字母，所以可以用数组来分别记录母串和子串出现的相应词频
+ */
+var findAnagrams2 = function (s, p) {
+  const sArray = new Array(26).fill(0);
+  const pArray = new Array(26).fill(0);
+  const res = [];
+  for (let i = 0; i < p.length; i++) {
+    pArray[p.charCodeAt(i) - 97]++;
+    sArray[s.charCodeAt(i) - 97]++;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (isAnagram(sArray, pArray)) {
+      res.push(i);
+    }
+    sArray[s.charCodeAt(i) - 97]--;
+    sArray[s.charCodeAt(i + p.length) - 97]++;
+  }
+  return res;
+};
+
+const isAnagram = (sArray, pArray) => {
+  for (let i = 0; i < pArray.length; i++) {
+    if (sArray[i] !== pArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+
+// console.log(findAnagrams2('Abab', 'Aab'))
+
+
+
+
